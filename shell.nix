df0657ae2a0556e31b46578a29a305eb93917704
Markus Ankenbrand <markus@ankenbrand.me>
2018-10-08 14:18:23 +0200

    Add Getopt::ArgvFile to nix shell

--- shell.nix
+++ shell.nix
@@ -1,8 +1,15 @@
 with import <nixpkgs> {};
-
+let GetoptArgvFile = buildPerlPackage rec {
+  name = "Getopt-ArgvFile-1.11";
+  src = fetchurl {
+    url = "https://cpan.metacpan.org/authors/id/J/JS/JSTENZEL/${name}.tar.gz";
+    sha256 = "08jvhfqcjlsn013x96qa6paif0095x6y60jslp8p3zg67i8sl29p";
+  };
+};
+in
 stdenv.mkDerivation {
   name = "reference_db_creator";
-  buildInputs = [ perlPackages.TestScript perl ];
+  buildInputs = [ perlPackages.TestScript GetoptArgvFile perl ];
   shellHook = ''
     export PERL5LIB=$PWD/lib:$PERL5LIB
   '';

fab9bf484ffe469c498f4fff7ec0ce82c52c9cf9
Markus Ankenbrand <markus@ankenbrand.me>
2018-10-16 17:53:54 +0200

    Add R script for plant family list via R as potential helper

---
+++ bin/european_plant_families.R
@@ -0,0 +1,32 @@
+# R script by Andreas Kolter to get european plant families via gbif
+
+european_plant_families <- function(pl_family_unique)
+{
+  library(rgbif)
+ 
+  fam_u <- pl_family_unique
+  keeper <- vector(length=length(fam_u),mode="logical")
+  #http://arthur-e.github.io/Wicket/sandbox-gmaps3.html
+  WKT_europe <- "POLYGON((3.427734375 56.69485488778099,18.28125 56.69485488778099,18.28125 43.75839933647409,3.427734375 43.75839933647409,3.427734375 56.69485488778099))"
+ 
+ 
+  for (i in 1:length(fam_u))
+  {
+    family_id <- name_backbone(name=fam_u[i],kingdom="Plantae")$usageKey
+    
+    #check occurrences in europe
+    germany_neigh <- occ_data(taxonKey=family_id,geometry=WKT_europe,hasCoordinate = T,
+                              limit=0,hasGeospatialIssue=F)$meta$count
+    if (germany_neigh<10) next
+    
+    #check globally
+    all_occ <- occ_data(taxonKey=family_id,hasCoordinate=T,limit=0,hasGeospatialIssue=F)$meta$count
+    
+    if (all_occ<10) next
+    
+    if ( (germany_neigh/all_occ>0.1)|(germany_neigh>100) ) keeper[i] <- T
+   if (i %% 10 == 0) print(i)
+  }
+  fam_u[keeper%>%as.logical]
+        
+}
